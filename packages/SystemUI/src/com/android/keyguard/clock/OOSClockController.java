dace4c35b89a9516caea06b2f1826e8b1a22344b
Matt Filetto <matt.filetto@gmail.com>
2022-06-22 15:19:27 -0700

    [DNM] Custom Clocks: Fix positions and overlaps [ P6p only ]
    # Conflicts:
    #	packages/SystemUI/res-keyguard/layout/clock_clockertino.xml
    #	packages/SystemUI/res-keyguard/layout/digital_clock_oos_big.xml
    #	packages/SystemUI/res-keyguard/layout/digital_clock_sfuny_big.xml
    #	packages/SystemUI/src/com/android/keyguard/clock/OOSClockController.java

--- packages/SystemUI/src/com/android/keyguard/clock/OOSClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/OOSClockController.java
@@ -117,7 +117,7 @@ public class OOSClockController implements ClockPlugin {
 
     
     public int getPreferredY(int totalHeight) {
-        return totalHeight / 5;
+        return totalHeight / 2;
     }
 
     
