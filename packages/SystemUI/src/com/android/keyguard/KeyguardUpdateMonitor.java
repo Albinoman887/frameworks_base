4173be5552e3841f2ee398e02b548916d807174e
Matt Filetto <matt.filetto@gmail.com>
2022-11-15 02:12:05 -0800

    Revert "Reset face auth on occluding app when authenticated"
    This reverts commit f2f266b82ff550c83c44e0ed962602bb08ca00dd.

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -822,9 +822,6 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
 
     private void handleFingerprintAuthenticated(int authUserId, boolean isStrongBiometric) {
         Trace.beginSection("KeyGuardUpdateMonitor#handlerFingerPrintAuthenticated");
-        if (mOccludingAppRequestingFace){
-            requestFaceAuthOnOccludingApp(false);
-        }
         if (mHandler.hasCallbacks(mFpCancelNotReceived)) {
             Log.d(TAG, "handleFingerprintAuthenticated()"
                     + " triggered while waiting for cancellation, removing watchdog");
@@ -1039,9 +1036,6 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
 
     private void handleFaceAuthenticated(int authUserId, boolean isStrongBiometric) {
         Trace.beginSection("KeyGuardUpdateMonitor#handlerFaceAuthenticated");
-        if (mOccludingAppRequestingFace){
-            requestFaceAuthOnOccludingApp(false);
-        }
         try {
             if (mGoingToSleep) {
                 Log.d(TAG, "Aborted successful auth because device is going to sleep.");
