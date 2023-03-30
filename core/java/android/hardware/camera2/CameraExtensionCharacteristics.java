f5b264720801091a2b3d0b1d5c849110f26de279
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-03-30 13:33:40 +0300

    allow "Pixel Camera Services" app to provide vendor camera extensions
    Since it's connected to directly by apps that use vendor camera extensions, it needs to be
    force-queryable.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/hardware/camera2/CameraExtensionCharacteristics.java
+++ core/java/android/hardware/camera2/CameraExtensionCharacteristics.java
@@ -42,6 +42,8 @@ import android.util.Pair;
 import android.util.Range;
 import android.util.Size;
 
+import com.android.internal.util.PixelCameraServicesUtils;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
@@ -241,6 +243,21 @@ public final class CameraExtensionCharacteristics {
 
         public static CameraExtensionManagerGlobal get() {
             return GLOBAL_CAMERA_MANAGER;
+        }
+
+        private static boolean validateVendorCameraExtensionsPackage(Context ctx, String pkgName) {
+            if (PixelCameraServicesUtils.validatePackage(ctx, pkgName)) {
+                return true;
+            }
+
+            try {
+                ApplicationInfo ai = ctx.getPackageManager().getApplicationInfo(pkgName,
+                        ApplicationInfoFlags.of(0L));
+                return ai.isSystemApp();
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+                return false;
+            }
         }
 
         private void connectToProxyLocked(Context ctx) {
