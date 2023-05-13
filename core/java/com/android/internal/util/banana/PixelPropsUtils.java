94c633feb97413889a62d547cbf39aeef5a1dd30
Matt Filetto <matt.filetto@gmail.com>
2023-05-13 05:13:19 -0700

    Revert "add gphots spoof"
    This reverts commit f3f444881174b93d2b66dd23181a596176b03fd0.

--- core/java/com/android/internal/util/banana/PixelPropsUtils.java
+++ core/java/com/android/internal/util/banana/PixelPropsUtils.java
@@ -175,10 +175,7 @@ public class PixelPropsUtils {
                 }
             }
             boolean useFallbackFp = false;
-            if (packageName.equals("com.google.android.apps.photos")) {
-                if (SystemProperties.getBoolean("persist.sys.pixelprops.gphotos", true)) {
-                sPXLProps.forEach((k, v) -> setPropValue(k, v));
-            } else if (!sCertifiedFp.isEmpty() && (sIsGms || sIsFinsky)) {
+            if (!sCertifiedFp.isEmpty() && (sIsGms || sIsFinsky)) {
                 dlog("Setting certified fingerprint for: " + packageName);
                 setPropValue("FINGERPRINT", sCertifiedFp);
             } else if (!sStockFp.isEmpty() && packageName.equals(PACKAGE_ARCORE)) {
