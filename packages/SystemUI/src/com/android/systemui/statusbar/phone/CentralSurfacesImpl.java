d9be35aa91d12ede18daf7177dc1dcfb1064f9a4
Matt Filetto <matt.filetto@gmail.com>
2023-05-12 20:19:01 -0700

    Revert "Ambient Music Ticker - Allow to pulse on new tracks [1/2]"
    This reverts commit 2bd1d65c5bdc3e2a3fe263cb5e518ee289db084f.
    
    # Conflicts:
    #	packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -162,7 +162,6 @@ import com.android.systemui.fragments.ExtensionFragmentListener;
 import com.android.systemui.fragments.FragmentHostManager;
 import com.android.systemui.fragments.FragmentService;
 import com.android.systemui.keyguard.KeyguardService;
-import com.android.systemui.keyguard.KeyguardSliceProvider;
 import com.android.systemui.keyguard.KeyguardUnlockAnimationController;
 import com.android.systemui.keyguard.KeyguardViewMediator;
 import com.android.systemui.keyguard.ScreenLifecycle;
@@ -286,8 +285,6 @@ import dagger.Lazy;
 @SysUISingleton
 public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, TunerService.Tunable {
 
-    private static final String PULSE_ON_NEW_TRACKS =
-            Settings.Secure.PULSE_ON_NEW_TRACKS;
     private static final String LESS_BORING_HEADS_UP =
             "system:" + Settings.System.LESS_BORING_HEADS_UP;
     private static final String RETICKER_STATUS =
@@ -957,7 +954,6 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
 
         mTunerService.addTunable(this, QS_TRANSPARENCY);
         mTunerService.addTunable(this, NOTIFICATION_MATERIAL_DISMISS);
-        mTunerService.addTunable(this, PULSE_ON_NEW_TRACKS);
         mTunerService.addTunable(this, LESS_BORING_HEADS_UP);
         mTunerService.addTunable(this, RETICKER_STATUS);
         mTunerService.addTunable(this, FORCE_SHOW_NAVBAR);
@@ -4224,13 +4220,6 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
             case NOTIFICATION_MATERIAL_DISMISS:
                 mShowDimissButton =
                         TunerService.parseIntegerSwitch(newValue, false);
-                break;
-            case PULSE_ON_NEW_TRACKS:
-                boolean showPulseOnNewTracks =
-                        TunerService.parseIntegerSwitch(newValue, false);
-                KeyguardSliceProvider sliceProvider = KeyguardSliceProvider.getAttachedInstance();
-                if (sliceProvider != null)
-                    sliceProvider.setPulseOnNewTracks(showPulseOnNewTracks);
                 break;
             case LESS_BORING_HEADS_UP:
                 boolean lessBoringHeadsUp =
