60382cb9f3136ef636e004219dcab4c75fefbc83
Matt Filetto <matt.filetto@gmail.com>
2023-05-16 23:50:28 -0700

    Revert "allow "Pixel Camera Services" app to provide vendor camera extensions"
    This reverts commit f5b264720801091a2b3d0b1d5c849110f26de279.

--- core/java/android/hardware/camera2/CameraExtensionCharacteristics.java
+++ core/java/android/hardware/camera2/CameraExtensionCharacteristics.java
@@ -42,8 +42,6 @@ import android.util.Pair;
 import android.util.Range;
 import android.util.Size;
 
-import com.android.internal.util.PixelCameraServicesUtils;
-
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
@@ -243,21 +241,6 @@ public final class CameraExtensionCharacteristics {
 
         public static CameraExtensionManagerGlobal get() {
             return GLOBAL_CAMERA_MANAGER;
-        }
-
-        private static boolean validateVendorCameraExtensionsPackage(Context ctx, String pkgName) {
-            if (PixelCameraServicesUtils.validatePackage(ctx, pkgName)) {
-                return true;
-            }
-
-            try {
-                ApplicationInfo ai = ctx.getPackageManager().getApplicationInfo(pkgName,
-                        ApplicationInfoFlags.of(0L));
-                return ai.isSystemApp();
-            } catch (PackageManager.NameNotFoundException e) {
-                e.printStackTrace();
-                return false;
-            }
         }
 
         private void connectToProxyLocked(Context ctx) {
