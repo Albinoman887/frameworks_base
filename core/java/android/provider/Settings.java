ad9002f2a0df866fa159d482f70a990264453219
Matt Filetto <matt.filetto@gmail.com>
2023-05-25 02:45:25 -0700

    SystemUI: Allow to change QS tile label text size [1/2]
    * Useful when using more than default columns.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    # Conflicts:
    #	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6159,6 +6159,11 @@ public final class Settings {
         public static final String QS_TILE_LABEL_HIDE = "qs_tile_label_hide";
 
         /**
+         * @hide
+         */
+        public static final String QS_TILE_LABEL_SIZE = "qs_tile_label_size";
+
+        /**
          * Adaptive playback
          * Automatically pause media when the volume is muted and
          * will resume automatically when volume is restored.
