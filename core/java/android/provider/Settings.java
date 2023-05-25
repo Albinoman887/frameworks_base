8ad242518e51828188f0d4e867828166ddc55838
Matt Filetto <matt.filetto@gmail.com>
2023-05-25 02:43:50 -0700

    SystemUI: Add QS tile layout settings [1/2]
    @neobuddy89:
    * Removed unused configs and imports
    * De-omnify with TunerService and TileUtils
    * Updated for A13 QPR1
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6597,6 +6597,23 @@ public final class Settings {
          */
         public static final String QS_TILE_ANIMATION_INTERPOLATOR = "qs_tile_animation_interpolator";
 
+
+
+        /**
+         * @hide
+         */
+        public static final String QS_LAYOUT_COLUMNS = "qs_layout_columns";
+
+        /**
+         * @hide
+         */
+        public static final String QS_TILE_VERTICAL_LAYOUT = "qs_tile_vertical_layout";
+
+        /**
+         * @hide
+         */
+        public static final String QS_TILE_LABEL_HIDE = "qs_tile_label_hide";
+
         /**
           * Statusbar Battery %
           * 0: Hide the battery percentage
